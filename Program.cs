f49cf3e8f1fac337fd9619d2417febd37bd25e9a
Kyrylo <112381397+KyryloStroiev@users.noreply.github.com>
2022-10-10 12:57:12 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,39 +1,39 @@
-﻿
-Console.WriteLine("Ведiть два числа.");
-int numb1, numb2;
-string stringNumb1 = Console.ReadLine();
-numb1 = Convert.ToInt32(stringNumb1);
-string stringNumb2 = Console.ReadLine();
-numb2 = Convert.ToInt32(stringNumb2);
 
-if (numb1 < numb2)
+bool summ()
 {
+   
     int summ = 0;
-    for (int i = numb1 ; i <= numb2; i++)
+    if (a < b)
     {
-        summ += i;
+
+        for (int i = a; i <= b; i++)
+        {
+            summ += i;
+        }
+
     }
-    Console.WriteLine(summ);
-}
-else if (numb1 > numb2)
-{
-    int summ = 0;
-    for (int i = numb2; i <= numb1; i++)
+    else if (a > b)
     {
-        summ += i;
+
+        for (int i = b; i <= a; i++)
+        {
+            summ += i;
+        }
     }
-    Console.WriteLine(summ);
+    if (summ % 2 == 0)
+    {
+        Console.WriteLine(true);
+        
+    }
+    else
+    {
+        Console.WriteLine(false);
+        
+    }
+    return;
 }
-else if (numb1 == numb2)
-{
-    Console.WriteLine(numb1);
-}
+Console.WriteLine(summ());
 
-
-
-
-
-
-
-
-
+   
+   
+   
