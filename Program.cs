de0495bd578cb459433e29247e312ed99e406e61
Kyrylo <112381397+KyryloStroiev@users.noreply.github.com>
2022-10-11 12:19:35 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
+using System;
+
+
+int a = 6, b = 8;
+
+int max()
+{
+    return Math.Max(a, b);
+}
+Console.WriteLine($"Максимальне число {max()}");
+int min()
+{
+    return Math.Min(a, b);
+}
+Console.WriteLine($"Мiнiмальне число {min()}");
+
 
 bool summ()
 {
@@ -22,18 +38,41 @@ bool summ()
     }
     if (summ % 2 == 0)
     {
-        Console.WriteLine(true);
-        
+        return (true);
     }
     else
     {
-        Console.WriteLine(false);
-        
+        return (false);
     }
-    return;
 }
 Console.WriteLine(summ());
 
-   
-   
-   
+public class Test
+{
+    int a = 3, b = 5;
+    int max()
+    {
+        return Math.Max(a, b);
+        Console.WriteLine($"Максимальне число {max()}");
+    }
+    int max(int c = 4, int d = 5)
+    {
+        return Math.Max(Math.Max(a,b), Math.Max(c,d));
+        Console.WriteLine($"Максимальне число {max()}");
+    }
+  
+    int min()
+    {
+        return Math.Max(a, b);
+        Console.WriteLine($"Максимальне число {max()}");
+    }
+    int min(int c = 7, int d = 9)
+    {
+        return Math.Max(Math.Max(a, b), Math.Max(c, d));
+        Console.WriteLine($"Максимальне число {max()}");
+    }
+
+
+
+
+}
